52f51ab7d44cfdb4004c2f65da20fe2d4a621f95
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-09-25 20:35:39 +0100

    Updated WeatherApp.py

--- weatherApp.py
+++ weatherApp.py
@@ -1,13 +1,17 @@
 # Weather App
-
 #Api_key = 3273a6c7e829f1ec80c2549464a55454
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, 
+                             QLineEdit, QPushButton)
 
 class WeatherApp(QWidget):
   def __init__(self):
     super().__init__()
+    self.city_label = QLabel("Enter city name: ", self)
+    self.city_input = QLineEdit(self)
+    self.get_weather_button = QPushButton("Get Weather", self)
+    self.temperature_label = QLabel("70°F", self)
 
 def main():
   app = QApplication(sys.argv)
